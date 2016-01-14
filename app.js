d9560865df10a55acb085c26c34ac7fe8f86b1f8
Erin Roby <erin.a.roby@gmail.com>
2016-01-13 22:10:54 -0800

    minor edits and comments removed

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
+//D.R.Y class-04
 var userName = prompt("Hello there! What's your name?");
 document.getElementById("greeting").innerHTML = "Hello, " + userName + ", very nice to meet you! Let's play a game to break the ice.";
 console.log("This is the userName variable: " + userName);
-var finalScore = 0; //note: I worked with Nassir and David to arrive at the counter solution.
+var finalScore = 0;
 
 var questions = ["Please guess yes or no: Am I from South Dakota?", "Do I believe in ghosts?", "Do you think I moonlight as a jazz musician?", "Okay, next question: Please guess a number between 1 and 10."];
 var answers = ["yes", "yes", "no", 2];
 var responses = ["I am from South Dakota!", "I do believe in ghosts! Don't you?", "I do not moonlight as a jazz musician.", ["You're correct!", "Too high!", "Too low!"]];
 var ids = ["a1", "a2", "a3", "a4"];
 
-function game(question, answer, response, id) { //Declaring a function named 'game' with 3 arguments: question, the question to ask. answer, the correct answer. response, the appropriate response, but when the answer is a number, the response is an array.
-  var ques = prompt(question).toLowerCase(); //Calling prompt and passing question then converting the returned string to lower case, then assign the result to variable ques.
+function game(question, answer, response, id) {
+  var ques = prompt(question).toLowerCase();
   if (answer === ques) {
     document.getElementById(id).innerHTML = "Good job! " + response;
     finalScore++;
